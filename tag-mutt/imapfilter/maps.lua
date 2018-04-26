acf5484bf744a5bb2db3b162750df6f4db346e87
Robert DJ <git@dahl-jacobsen.dk>
2018-04-26 08:54:09 +0200

    First set of filtering rules

---
+++ tag-mutt/imapfilter/maps.lua
@@ -0,0 +1,66 @@
+function trim(str)
+  return str:gsub('^%s+', ''):gsub('%s+$', '')
+end
+
+-- Get the password by reading the output of the gpg command used in the other
+-- Mutt related config files
+function get_imap_password(file)
+	local command = "gpg2 -q --for-your-eyes-only --no-tty -d " .. file
+	local f = io.popen(command)
+	local password = f:read("*a")
+	f:close()
+	return trim(password)
+end
+
+local p = get_imap_password("~/.mutt/accounts/maps.gpg")
+
+local account = IMAP {
+	server = 'imap.one.com',
+	username = 'maps@dahl-jacobsen.dk',
+	password = get_imap_password("~/.mutt/accounts/maps.gpg"),
+	ssl = 'auto'
+}
+
+inbox = account['INBOX']
+inbox:check_status()
+
+-- messages = 
+-- 	inbox:match_from('.*@facebookmail.com') *
+-- 	(
+-- 		inbox:contain_subject('Robert, did you see') +
+-- 		inbox:contain_subject('shared a link') +
+-- 		inbox:match_subject('updated (his|her) status') +
+-- 		inbox:match_subject('comment on (his|her) status') +
+-- 		inbox:match_subject('tagged.* in a photo') +
+-- 		inbox:match_subject('shared .* post') +
+-- 		inbox:match_subject('added a new (photo|video)')
+-- 	)
+-- messages:mark_seen()
+-- messages:move_messages(account['Facebook'])
+
+-- messages = inbox:match_from('.*@facebookmail.com')
+-- messages:move_messages(account['Facebook'])
+
+-- messages = inbox:match_from('.*@linkedin.com')
+-- messages:move_messages(account['LinkedIn'])
+
+-- messages = inbox:match_from('.*@noreply.github.com')
+-- messages:move_messages(account['GitHub'])
+
+-- messages = inbox:match_from('.*@kokkenshverdagsmad.dk')
+-- messages:move_messages(account['Madkasse'])
+
+-- messages = inbox:match_from('noreply@rejsekort.dk')
+-- messages:move_messages(account['Rejsekort'])
+
+-- messages = inbox:match_from('noreply@oister.dk')
+-- messages:move_messages(account['Oister'])
+
+-- messages = inbox:match_from('julialang@discoursemail.com')
+-- messages:move_messages(account['Forum'])
+
+-- messages = 
+-- 	inbox:match_from('e-medd@dsts.dk') +
+-- 	inbox:match_subject('^[DMF]')
+-- messages:move_messages(account['DSTS'])
+
