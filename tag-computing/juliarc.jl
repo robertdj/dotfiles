37080ea2d26a319ca62979b6389c07608f5c758a
Robert DJ <git@dahl-jacobsen.dk>
2018-06-18 14:27:52 +0200

    Julia setup

--- tag-computing/juliarc.jl
+++ tag-computing/juliarc.jl
@@ -1,3 +1,9 @@
 # Extend path
 push!(LOAD_PATH, homedir() * "/Dropbox/julia")
 
+ENV["EDITOR"] = "vim"
+
+# REPL colors
+ENV["JULIA_STACKFRAME_LINEINFO_COLOR"] = :cyan;
+ENV["JULIA_STACKFRAME_FUNCTION_COLOR"] = :yellow;
+
