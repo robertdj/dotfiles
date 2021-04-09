cee550f7d311a4c5967f3ed498ceafb0e53c9d47
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-04-09 22:48:01 +0200

    Semicolons seem unnecesarry

--- tag-computing/julia/config/startup.jl
+++ tag-computing/julia/config/startup.jl
@@ -4,6 +4,6 @@ push!(LOAD_PATH, homedir() * "/Documents/julia")
 ENV["EDITOR"] = "vim"
 
 # REPL colors
-ENV["JULIA_STACKFRAME_LINEINFO_COLOR"] = :cyan;
-ENV["JULIA_STACKFRAME_FUNCTION_COLOR"] = :yellow;
+ENV["JULIA_STACKFRAME_LINEINFO_COLOR"] = :cyan
+ENV["JULIA_STACKFRAME_FUNCTION_COLOR"] = :yellow
 
