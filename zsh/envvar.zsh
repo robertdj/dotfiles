f052bd382ad1513c7c51103c44b0d3d3ceb42693
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2022-03-28 19:55:43 +0200

    🔧 Use R 4.1.2

--- zsh/envvar.zsh
+++ zsh/envvar.zsh
@@ -10,8 +10,8 @@ export MANPATH="/usr/local/texlive/2018/texmf-dist/doc/man:$MANPATH"
 export EDITOR="vim"
 export VIMHOME="$HOME/.vim"
 
-export R_HOME="/opt/R/4.1.1/lib/R"
-export LD_LIBRARY_PATH="/opt/R/4.1.1/lib/R/lib:$LD_LIBRARY_PATH"
+export R_HOME="/opt/R/4.1.2/lib/R"
+export LD_LIBRARY_PATH="/opt/R/4.1.2/lib/R/lib:$LD_LIBRARY_PATH"
 
 export JULIA_NUM_THREADS=4
 
