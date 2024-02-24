3c12170253ab7060be59ac77c1524ca4a8527513
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2024-02-24 20:07:13 +0100

    🧑‍💻 Alias to activate default Python venv

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,4 +2,4 @@ alias ..='cd ..'
 alias ...='cd ../..'
 
 # Get DHCP leases from EdgeOS router
-alias leases='ssh edge "/opt/vyatta/bin/vyatta-op-cmd-wrapper show dhcp leases"'
+alias venv_activate='source .venv/bin/activate'
